59f4664e995820806b00840433357d09b3cfae80
Anti <kaybart@hotmail.de>
2022-07-03 22:02:28 +0200

    feat: add skutek db

--- data_class.py
+++ data_class.py
@@ -1,12 +1,66 @@
-from tkinter import * 
+from tkinter import *
+from peewee import *
+
+db = SqliteDatabase('skutek.db')
+
+
+
+class Scooters(Model):
+    id = AutoField()
+    name = CharField()
+    price_distance = DoubleField()
+    price_time = DoubleField()
+    unlock_fee = DoubleField()
+
+    class Meta:
+        database = db
+
+class Calculations(Model):
+    id = AutoField()
+    scooter = ForeignKeyField(Scooters)
+    timestamp = DateTimeField()
+    is_distance_calculation = BooleanField()
+    value = DoubleField()
+    include_unlock_fee = BooleanField()
+
+    class Meta:
+        database = db
+        
+
+
+
+
 
 class bfunctions():
-    def __init__(self):
-        self.show
-        
+
     def exit():
         exit()
     """Function to exit the program per Button"""
     
     def show():
         label.config(text=clicked.get())
+    
+#Main Vars
+options = [
+    "kunde",
+    "bestellung",
+    "lieferant",
+    "zutat",
+    "bestellungszutat"
+]
+
+options2 = [
+    "CUNT",
+    "C C",
+    "U U",
+    "N N",
+    "T T"
+]
+
+options3 = [
+    "kunde",
+    "bestellung",
+    "lieferant",
+    "zutat",
+    "bestellungszutat"
+]
--- mainWindow.py
+++ mainWindow.py
@@ -1,44 +1,35 @@
-from data_class import bfunctions
+from data_class import *
 from tkinter import *
+
+
+
+
+db.connect()
+db.create_tables([Scooters, Calculations])
+
+if not Scooters.select().where(Scooters.name == 'Tier S').exists():
+    Scooters.create(name='Tier S', price_distance=20, price_time=50, unlock_fee=1.33)
+if not Scooters.select().where(Scooters.name == 'Tier M').exists():
+    Scooters.create(name='Tier M', price_distance=30, price_time=77, unlock_fee=2.33)
+if not Scooters.select().where(Scooters.name == 'Tier L').exists():
+    Scooters.create(name='Tier L', price_distance=40, price_time=100, unlock_fee=0.20)
+
+
+scooters = Scooters.select()
 
 fenster = Tk()
 fenster.rowconfigure(0,weight=2)
 fenster.rowconfigure(1,weight=1)
 fenster.rowconfigure(2,weight=1)
-fenster.title("Databank_KrautundRüben")
+fenster.title("ScooTeC")
 fenster.geometry("500x400")
 fenster.minsize(width=500, height=400)
 
-options = [
-    "kunde",
-    "bestellung",
-    "lieferant",
-    "zutat",
-    "bestellungszutat"
-]
 
-options2 = [
-    "CUNT",
-    "C C",
-    "U U",
-    "N N",
-    "T T"
-]
+# Info Label
+label = Label(fenster, text="ScooTeq\n Program to calculate the Price for your Duration or Distance driven.")
 
-options3 = [
-    "kunde",
-    "bestellung",
-    "lieferant",
-    "zutat",
-    "bestellungszutat"
-]
-
-# Create Label
-label = Label(fenster, text="\n\nWelcome to the Databank of Kraut und Rüben\n\n"
-                            "Here you can have look at the different kategories of the database Kraut und Rüben\n"
-                            "you can take a look at different tabels or even save them as a textdokument.\n.\n"
-                            "To get started select a kategory and press the button\n")
-
+#button variables
 clicked = StringVar()
 clicked.set("1.Kategories")
 clicked2 = StringVar()
@@ -46,16 +37,19 @@ clicked2.set("2.Tabels")
 clicked3 = StringVar()
 clicked3.set("3.Textdocs")
 
+#Widgets
 b_kategories = OptionMenu(fenster, clicked, *options)
 b_tabels = OptionMenu(fenster, clicked2, *options2)
 b_textdoc = OptionMenu(fenster, clicked3, *options3)
 b_exit = Button(fenster, text="Exit", command="exit")
-button = Button(fenster, text="click Me")
+button = Button(fenster, text="click Me", command="show")
 
+#Widget builder
 label.grid(padx=20, columnspan=3)
 b_kategories.grid(row=1)
 b_tabels.grid(row=1,column=1)
 b_textdoc.grid(row=1, column=2)
+button.grid(row=2,column=1)
 b_exit.grid(row=2, column=2)
 
 fenster.mainloop()
